8a1228cebfbab9c223a992b932490a4f321e1d12
KidpaddleetcieGlin <37848836+Kidpaddleetcie@users.noreply.github.com>
2019-10-16 11:01:38 +0200

    Initialisation
    Le cadre est créé et les liens JS,CSS et HTML a été fait !

---
+++ assets/js/declaration.js
@@ -0,0 +1,22 @@
+// Liste des personnages
+const perso=["Personne","Layton","Luke","Descole","Pr. Luigi","Tintin","Glin","Kass","Hitler"];
+
+// Liste émotions
+const emotion=["Neutre","Heureux","Triste","Bruh"];
+
+// Liste décors
+const decor=["Noir","Chambre","Salle","Toilette","Paris","Voiture","Bureau","Hopital"];
+
+// Déclaration de la zone 
+const screen= document.getElementById("background");
+const tb= document.getElementById("textbox");
+let name=document.getElementById("name");
+
+
+// Boutons 3 scènes
+const b1=document.getElementById("b1");
+const b2=document.getElementById("b2");
+const b3=document.getElementById("b3");
+
+
+
---
+++ assets/js/executif.js
@@ -0,0 +1,14 @@
+// Boutons 1,2 et 3
+b1.addEventListener("click",scene1);
+b2.addEventListener("click",scene2);
+b3.addEventListener("click",scene3);
+
+function scene1(){
+
+}
+function scene2(){
+
+}
+function scene3(){
+    
+}
